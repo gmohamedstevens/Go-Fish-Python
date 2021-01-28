e746304bac2cfa8f778ede9c9e3d7e35f4a4ed95
Gamal Mohamed <gmohamed@stevens.edu>
2021-01-28 13:16:54 -0500

    Aded functions for discarding cards and detecting cards by suit/value

--- hand.py
+++ hand.py
@@ -1,7 +1,7 @@
 from deck import *
+from card import *
 
 # Hand contains
-# Hand size
 
 class Hand(object):
     def __init__(self):
@@ -12,7 +12,7 @@ class Hand(object):
         for card in self.hand_list:
             print(card.to_string())
 
-    # Draw a card from the deck and add it to the hand list
+    # Draw a card from the deck and add it to the hand
     def draw_from_deck(self, deck, number_of_cards):
         try:
             for i in range(number_of_cards):
@@ -22,14 +22,36 @@ class Hand(object):
             print("The deck is empty, no more cards can be drawn")
         self.sort_by_value()
 
-    # Add a card to the hand list
+    # Add a card to the hand
     def add_card(self, card):
         self.hand_list.append(card)
 
-    def discard_card(self, number_of_cards):
+    def has_card(self, suit, value):
         pass
 
-    # Perform selection sort on hand list based on card suit
+    # Discard all cards of a specified value
+    def discard_by_value(self, value):
+        for index in range(len(self.hand_list)-1 , -1, -1):
+            card = self.hand_list[index]
+            if card.get_value_int() == value:
+                self.hand_list.remove(card)
+
+    # Returns the card count of a specified value in the hand
+    def count_by_value(self, value):
+        count = 0
+        for card in self.hand_list:
+            if card.get_value_int() == value:
+                count += 1
+        return count
+
+    # Returns if a four of a kind of the secified value exists in the hand
+    def four_of_a_kind_exists(self, value):
+        if self.count_by_value(value) == 4:
+            return True
+        else:
+            return False
+
+    # Perform selection sort on hand on card suit
     def sort_by_suit(self):
         for i in range(len(self.hand_list)):
             min_index = i
@@ -40,7 +62,7 @@ class Hand(object):
             self.hand_list[i] = self.hand_list[min_index]
             self.hand_list[min_index] = temp
 
-    # Perform selection sort on hand list based on card value
+    # Perform selection sort on hand based on card value
     def sort_by_value(self):
         for i in range(len(self.hand_list)):
             min_index = i
@@ -56,6 +78,6 @@ class Hand(object):
     # GETTER METHODS #
     ##################
 
-    # Return size of hand list
+    # Return size of hand
     def get_size(self):
         return len(hand_list)
