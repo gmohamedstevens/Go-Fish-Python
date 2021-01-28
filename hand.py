4fce49e719abbf8c3f97b2626f87b04447b41028
Gamal Mohamed <gmohamed@stevens.edu>
2021-01-28 10:51:17 -0500

    Added comments to Hand class

--- hand.py
+++ hand.py
@@ -7,10 +7,12 @@ class Hand(object):
     def __init__(self):
         self.hand_list = []
 
+    # Print the contents of the hand
     def display_hand(self):
         for card in self.hand_list:
             print(card.to_string())
 
+    # Draw a card from the deck and add it to the hand list
     def draw_from_deck(self, deck, number_of_cards):
         try:
             for i in range(number_of_cards):
@@ -20,12 +22,14 @@ class Hand(object):
             print("The deck is empty, no more cards can be drawn")
         self.sort_by_value()
 
+    # Add a card to the hand list
     def add_card(self, card):
         self.hand_list.append(card)
 
     def discard_card(self, number_of_cards):
         pass
 
+    # Perform selection sort on hand list based on card suit
     def sort_by_suit(self):
         for i in range(len(self.hand_list)):
             min_index = i
@@ -36,6 +40,7 @@ class Hand(object):
             self.hand_list[i] = self.hand_list[min_index]
             self.hand_list[min_index] = temp
 
+    # Perform selection sort on hand list based on card value
     def sort_by_value(self):
         for i in range(len(self.hand_list)):
             min_index = i
@@ -50,7 +55,7 @@ class Hand(object):
     ##################
     # GETTER METHODS #
     ##################
-    
+
+    # Return size of hand list
     def get_size(self):
         return len(hand_list)
-    
